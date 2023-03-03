afa0bf5a9cc1f30354d7233de092358bb56fd85a
0704Muthu <rmuthukumar0704@gmail.com>
2023-03-03 15:31:27 +0530

    secondBuild

---
+++ src/org/Data/DataTypes.java
@@ -0,0 +1,19 @@
+package org.Data;
+
+public class DataTypes {
+public static void main(String[] args) {
+	byte a=10;
+	short b =20;
+	System.out.println(a);
+	int c=30;
+	long d=178;
+	float e=2.5f;
+	double f=3.5654;
+	System.out.println(f);
+	System.out.println(e);
+	String f1=("acdajskj");
+	System.out.println(f1);
+} 
+	
+
+}
